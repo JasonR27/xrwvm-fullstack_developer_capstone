ad173fb963d7e828e8a5c84465c81596b6a4e2ac
Jeison Roblero <jeisonroblero980@gmail.com>
2025-01-20 22:20:42 -0600

    Added cars data

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -11,12 +11,15 @@ urlpatterns = [
 
     # path for login
     path(route='login', view=views.login_user, name='login'),
- 
+
     # path for dealer reviews view
 
     # path for add a review view
-    
+
     # path for logout view
     path(route='logout', view=views.logout_request, name='logout'),
+
+    # path for getting a list of all cars view
+    path(route='get_cars', view=views.get_cars, name='getcars'),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
