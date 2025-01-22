aae97eb8ba8042188e08b3ed1f67203dc457ac12
Jeison Roblero <jeisonroblero980@gmail.com>
2025-01-22 01:00:22 -0500

    added port-forwarding and configuration for containerazation to run in kubernetes

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -12,8 +12,8 @@ SECRET_KEY =\
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', 'http://127.0.0.1:3000/', '127.0.0.1']
-CSRF_TRUSTED_ORIGINS = ['http://127.0.0.1:3000/', 'http://127.0.0.1/']
+ALLOWED_HOSTS = ['localhost', 'http://127.0.0.1:3000/', '127.0.0.1', 'https://jeisonrobler-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/']
+CSRF_TRUSTED_ORIGINS = ['http://127.0.0.1:3000/', 'http://127.0.0.1/', 'https://jeisonrobler-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/']
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [],
